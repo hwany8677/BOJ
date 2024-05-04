82f0bc20340ebd3312f108417c95a6ce570736a4
hwany8677 <19703339+hwany8677@users.noreply.github.com>
2024-05-04 22:43:59 +0900

    5월 4일 귀찮은날

---
+++ 14581.py
@@ -0,0 +1,5 @@
+#아 ps하기귀찮은날
+s=input()
+print(":fan::fan::fan:")
+print(f":fan::{s}::fan:")
+print(":fan::fan::fan:")
--- 2857_unsolved.py
+++ 2857_unsolved.py
@@ -2,9 +2,9 @@ s=[]
 for i in range(0,5): s.append(input())
 c=[]
 for i in range(0,5):
-	for j in range(0,len(s[i])-2):
+	print(f"String length: {len(s[i])}")
+	for j in range(0,len(s[i])):
 		if s[i][j]=='F' and s[i][j+1]=='B' and s[i][j+2]=='I': c.append(i+1)
-print(c)
 if len(c):
 	for i in c: print(f"{i} ",end='')
 else: print("HE GOT AWAY!")

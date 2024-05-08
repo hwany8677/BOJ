a76c6156edfc3dc4d68b7be23f7dfbd44520a728
Hwany <19703339+hwany8677@users.noreply.github.com>
2024-05-08 18:22:02 +0900

    Minor changes

--- 11653.py
+++ 11653.py
@@ -1,5 +1,7 @@
+#Time complexity: O(sqrt(n)) (According to GPT)
+#Therefore: takes 1 seconds if n>=100,000,000
 def prime(n):
-	if (n==1): return;
+	if (n==1): return
 	i=2
 	while(n!=1):
 		if (n%i==0):

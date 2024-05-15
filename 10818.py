e11ab26e797754e3e54ce78237e139350b7f1768
hwany8677 <19703339+hwany8677@users.noreply.github.com>
2024-05-15 20:10:32 +0900

    스트릭을 유지할꺼에오 우히히

---
+++ 10818.py
@@ -0,0 +1,4 @@
+#시간지연도 12837461283746123배
+n=int(input())
+arr=list(map(int,input().split()))
+print(min(arr),max(arr))
--- 3040.py
+++ 3040.py
@@ -8,11 +8,17 @@ def calc(dw):
 	probe=[0,1,2,3,4,5,6]
 	for i in range(6,-1,-1):
 		for j in range(0,2):
-			s=add(probe,dw)	
-			if s==100: return probe
-			else: probe[i]+=1
-	return probe
+			print(f"Current probe state: {probe}")
+			s=add(probe,dw)
+			print(f"Probe's sum = {s}")	
+			if s==100: 
+				print("Match found!")
+				return probe
+			else: 
+				probe[i]+=1
+		print()
 dw=[]
 for _ in range(0,9): dw.append(int(input()))
+print(f"Drawfs: {dw}")
 probe=calc(dw)
 for n in probe: print(n)

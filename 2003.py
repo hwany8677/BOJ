0a1d81a0d815c3c7d6ebe38c5b64eedd5b59e6d7
hwany8677 <19703339+hwany8677@users.noreply.github.com>
2024-07-22 01:03:55 +0900

    7월 21일 집 (naive codes)

--- 2003.py
+++ 2003.py
@@ -1,3 +1,4 @@
+#Naive approach, O(n²)
 from sys import stdin
 input=stdin.readline
 def Sn(lisT):
@@ -7,13 +8,37 @@ def Sn(lisT):
     return res
 n,m=map(int,input().split())
 buf=list(map(int,input().split()))
-s=Sn(buf)
 count=0
-start,end=0,n-1
-while(end<n):
-    if start==0: sigma=s[start]
-    else: sigma=s[end]-s[start-1]
-    if sigma==m: count+=1
-    start+=1
-    end+=1
+S=Sn(buf) #구간합 base
+#4 2
+#1 1 1 1
+#try: 
+#S1, S2, S3, S4
+#S3-S1, S4-S2 (2 elements)
+#S4-S1 (3 elements)
+#
+#10 5
+#1 2 3 4 2 5 3 1 1 2
+#try:
+#S1, S2, ..., S10 (full seek)
+#S3-S1, S4-S2, ..., S10-S8 (2 elements)
+#S4-S1, S5-S2, ..., S10-S7 (3 elements)
+#...
+#S9-S1, S10-S2 (8 elements)
+#S10-S1 (9 elements)
+#-> end가 len(S) 넘어가면 각각 while문 끝내는걸로
+#새로운 end는 i+1로 (i=0 시작, end=1(제외),2,3,...)
+count=0
+length=len(S)
+for i in range(0,n):
+    start,end=0,i+1
+    if i==0: #S1, S2, ..., S10 + a1, a2, ..., a10 (full seek) 
+        for j in range(0,n): 
+            if buf[j]==m: count+=1
+            elif S[j]==m: count+=1
+        continue
+    while(end<length): 
+        if S[end]-S[start]==m: count+=1 #S3-S1, S4-S2, ..., S10-S8 
+        end+=1
+        start+=1
 print(count)
--- 2714.py
+++ 2714.py
@@ -1,3 +1,10 @@
+#5개씩 자르기
+#10157과 비슷하게 짜야함 (근데 소스를 못읽음 :skull::skull:)
+def seek(): 
+    mode=1
+    k=R*C
+    i=0
+    
 for _ in range(int(input())):
     buf=input().split()
     R,C=int(buf[0]),int(buf[1])
@@ -7,4 +14,7 @@ for _ in range(int(input())):
     while(i<len(buf)):
         mtrx.append(buf[i:i+C])
         i+=C
-    
+    visited_map=[]
+    for _ in range(R): visited_map.append([0 for _ in range(C)])
+    #방문여부를 알아야 턴을 하지
+    print(seek())

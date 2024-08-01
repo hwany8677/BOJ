1b431d48e1a94afcef3ef421f783d4c572503c3c
Hwany <19703339+hwany8677@users.noreply.github.com>
2024-08-01 12:05:15 +0000

    8월 1일 집 (GCC로 *nix 실행파일 만듦)

---
+++ 1000.c
@@ -0,0 +1,9 @@
+//Written in May 11th, 2018
+#include <stdio.h>
+int main(){
+    int a=0;
+    int b=0;
+    scanf("%d %d",&a,&b);
+    printf("%d",a+b);
+    return 0;
+}
---
+++ 1000_2.c
@@ -0,0 +1,9 @@
+//Old version exists
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char**argv){
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%d\n",a+b);
+	exit(0);
+}

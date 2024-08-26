4c0b496cfd217c47b4275a233c28c0291b7aa533
hwany8677 <19703339+hwany8677@users.noreply.github.com>
2024-08-27 04:13:58 +0900

    8월 26일 집 (30218을 죽인다)

---
+++ 16486.py
@@ -0,0 +1,4 @@
+d1=int(input())
+d2=int(input())
+pi=3.141592
+print("{:.6f}".format(d1*2+2*pi*d2))
--- 30218.py
+++ 30218.py
@@ -1,44 +1,38 @@
 from sys import stdin
 input=stdin.readline
 
-def order(lisT1, lisT2):
-    zp=zip(lisT2, lisT1)
-    res=[x for _, x in sorted(zp)]
-    return res
+# def order(od, s):
+#     zp=zip(s, od)
+#     res=[x for _, x in sorted(zp)]
+#     return res
 
-def Sn(lisT):
-    global od
-    c=2
-    res=[lisT[0]]
-    od.append(1)
-    for i in range(1,len(lisT)):
-        res.append(lisT[i]+res[i-1])
-        od.append(c)
-        c+=1
-    return res
+# def Sn_special(lisT,size):
+#     res=[]
+#     sigma=[(i+1)*lisT[i] for i in range(size)]
+#     res.append(sum(sigma))
+#     for j in range(1,len(lisT)-size+1):
+#         sigma=[(k+1)*lisT[i] for i,k in zip(range(j,j+size),range(size))]
+#         res.append(sum(sigma))
+#     return res
 
-def Sn_special(lisT,size):
-    global od
+def Sn_rewrite(lisT,size):
     res=[]
-    sigma=[(i+1)*lisT[i] for i in range(size)]
-    res.append(sum(sigma))
-    c=2
-    od.append(1)
-    for j in range(size-1,len(lisT)-1):
-        sigma=[lisT[i] for i in range(j-1,j+size-1)]
-        for k in range(size): sigma[k]=(k+1)*sigma[k]
-        res.append(sum(sigma))
-        od.append(c)
-        c+=1
+    for i in range(len(lisT)-size+1):
+        sigma=0
+        weight=1
+        for j in range(i,i+size): 
+            sigma+=weight*lisT[j]
+            weight+=1
+        res.append((sigma,i+1))
     return res
 
 n,k=map(int,input().split())
 buf=[]
-od=[]
-for _ in range(n):
-    buf.append(int(input()))
-s=Sn(buf) if k==1 else Sn_special(buf,k)
-od=order(od,s)
+# od=[i+1 for i in range(n-k+1)]
+for _ in range(n): buf.append(int(input()))
+# s=Sn_special(buf,k)
+s=Sn_rewrite(buf,k)
+# od=order(od,s)
 s.sort()
 for i in range(len(s)):
-    print(od[i],s[i])
+    print(s[i][1],s[i][0])

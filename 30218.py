4b2c2e65be97317396c08ba940da2cf35a89c6b6
hwany8677 <19703339+hwany8677@users.noreply.github.com>
2024-08-28 01:28:04 +0900

    8월 27일 집 (짧)

--- 30218.py
+++ 30218.py
@@ -1,38 +1,37 @@
+#1a+2b+3c
+#-> 1b+2c+3d를 할려면
+#1a+2b+3c - (a+b+c) + 3d를 해야하고
+#
+#1c+2d+3e를 할려면
+#1b+2c+3d - (b+c+d) + 3e를 해야한다
+#=> 애초에 식 정리를 해야되는 문제
 from sys import stdin
+from collections import deque
 input=stdin.readline
 
-# def order(od, s):
-#     zp=zip(s, od)
-#     res=[x for _, x in sorted(zp)]
-#     return res
+def Sn(lisT):
+    res=[lisT[0]]
+    for i in range(1,len(lisT)):
+        res.append(lisT[i]+res[i-1])
+    return res
 
-# def Sn_special(lisT,size):
-#     res=[]
-#     sigma=[(i+1)*lisT[i] for i in range(size)]
-#     res.append(sum(sigma))
-#     for j in range(1,len(lisT)-size+1):
-#         sigma=[(k+1)*lisT[i] for i,k in zip(range(j,j+size),range(size))]
-#         res.append(sum(sigma))
-#     return res
-
-def Sn_rewrite(lisT,size):
+def Sn_final(lisT,size):
+    global c_sum
     res=[]
-    for i in range(len(lisT)-size+1):
-        sigma=0
-        weight=1
-        for j in range(i,i+size): 
-            sigma+=weight*lisT[j]
-            weight+=1
-        res.append((sigma,i+1))
+    sigma=0
+    for i in range(len(lisT)-size):
+        left=i
+        right=i-len(lisT)+1
+        sigma-=c_sum[right]-c_sum[left-1]
+        sigma+=size*lisT[right+1]
+        res.append(sigma)
     return res
 
 n,k=map(int,input().split())
 buf=[]
-# od=[i+1 for i in range(n-k+1)]
 for _ in range(n): buf.append(int(input()))
-# s=Sn_special(buf,k)
-s=Sn_rewrite(buf,k)
-# od=order(od,s)
+c_sum=Sn(buf)
+s=Sn_final(buf,k)
 s.sort()
 for i in range(len(s)):
     print(s[i][1],s[i][0])

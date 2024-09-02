c1743cc53e9e470d09c56f89d311804bdadd1bdb
Hwany <19703339+hwany8677@users.noreply.github.com>
2024-09-02 22:14:22 +0900

    9월 2일 학교 3 (찐막)

---
+++ 5430.py
@@ -0,0 +1,28 @@
+from sys import stdin
+input=stdin.readline
+
+for _ in range(int(input())):
+    p=input()
+    n=input()
+    buf=list(map(int,input().strip('[').strip(']\n').split(',')))
+    #reverse 메소드는 O(n) -> 끝부분을 정하고 pop 써야됨
+    to_pop=0
+    broken=False
+    for cmd in p:
+        if cmd=='R': 
+            to_pop=0 if to_pop!=0 else len(buf)-1
+        elif cmd=='D':
+            try:
+                buf.pop(to_pop)
+                to_pop-=1 if to_pop!=0 else 0
+            except IndexError:
+                print("error")
+                broken=True
+                break
+    if broken: continue
+    if to_pop!=0: buf.reverse()
+    print('[',end='')
+    for i in range(len(buf)):
+        print(buf[i],end='')
+        if i<len(buf)-1: print(',',end='')
+    print(']')

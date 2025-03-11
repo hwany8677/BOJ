7b31a2b6bce69f17bf3db6e18c7cf5237a6fb585
hwany8677 <19703339+hwany8677@users.noreply.github.com>
2025-03-12 01:30:47 +0900

    3월 12일 집 (좌절)

--- 22232_unsolved.py
+++ 22232_unsolved.py
@@ -2,25 +2,26 @@
 #1. Filename (Lexi.)
 #2. Recognized Extensions (Lexi.)
 #3. Unrecognized Extensions (Lexi.)
+from collections import deque
 input=open(0).readline
 n,m=map(int,input().split())
 buf=dict()
-ext_known=[]
-ext_unknown=[]
+ext_known=deque()
+ext_unknown=deque()
 for _ in range(n):
     s=input().rstrip().split('.')
     name,ext=s[0],s[1]
-    if name not in buf: buf[name]=[ext]
+    if name not in buf: buf[name]=deque([ext])
     else: buf[name].append(ext)
     ext_unknown.append(ext)
-ext_unknown.sort()
+# ext_unknown.sort()
 for _ in range(m): 
     s=input().rstrip()
     if s in ext_unknown: ext_unknown.remove(s)
     ext_known.append(s)
-ext_known.sort()
-ext_keys=list(buf.keys())
-ext_keys.sort()
+# ext_known.sort()
+ext_keys=deque(buf.keys())
+# ext_keys.sort()
 buf={fname: buf[fname] for fname in ext_keys}
 for element in buf.keys():
     for elemento in buf[element]: #buf[element] -> where all extensions go = elemento

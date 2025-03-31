9a5db1c3bc3461d9bc3d37f6484671271a2bfed6
Hwany <19703339+hwany8677@user.noreply.github.com>
2025-03-31 10:34:35 +0900

    날짜 미상

--- 1011.py
+++ 1011.py
@@ -2,17 +2,25 @@
 #1~46342까지의 모든 경우를 따진다
 #46341^2, 46341*46342
 input=open(0).readline
+buf=[i**2 for i in range(1,46343)]
+buf2=[i*(i+1) for i in range(1,46344)]
+buf.sort()
+buf2.sort()
 for _ in range(int(input())):
     x,y=map(int,input().split())
     dist=y-x
-    buf=[i**2 for i in range(1,46342)]
-    buf2=[i*(i+1) for i in range(1,46343)]
-    final=sorted(buf+buf2)
     #dist>final[i] 인 경우 바로 멈춤
     i=0
+    res=0
     while(1):
-        if dist>final[i]:
-            i-=1
+        if buf[i]<dist and dist<buf2[i]:
+            print("broke",i,f"dist={dist}",f"final[i]={final[i]}")
+            i+=1
+            res=i+1
+            break
+        if buf2[i]<dist and dist<buf[i+1]:
+            print("broke",i,f"dist={dist}",f"final[i]={final[i]}")
+            
             break
         i+=1
-    print(final[i])
+    print()

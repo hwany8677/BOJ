b8c23b7bab1348bb31ff55614a50d7335dae6a6c
Hwany <19703339+hwany8677@user.noreply.github.com>
2025-03-31 20:42:45 +0900

    3월 31일 학교 (하루종일 센타우리 갈 궁리만 하는 ㅄ)

--- 1011.py
+++ 1011.py
@@ -1,26 +1,26 @@
 #산만함 MAX
 #1~46342까지의 모든 경우를 따진다
 #46341^2, 46341*46342
+#이진 탐색
 input=open(0).readline
 buf=[i**2 for i in range(1,46343)]
 buf2=[i*(i+1) for i in range(1,46344)]
-buf.sort()
-buf2.sort()
+final=[0]
+final+=buf; final+=buf2
+final.sort()
 for _ in range(int(input())):
     x,y=map(int,input().split())
     dist=y-x
-    #dist>final[i] 인 경우 바로 멈춤
-    i=0
+    low,high=1,46343
     res=0
     while(1):
-        if buf[i]<dist and dist<buf2[i]:
-            print("broke",i,f"dist={dist}",f"final[i]={final[i]}")
-            i+=1
-            res=i+1
+        mid=(low+high)//2
+        if dist==final[mid]:
+            res=mid
             break
-        if buf2[i]<dist and dist<buf[i+1]:
-            print("broke",i,f"dist={dist}",f"final[i]={final[i]}")
-            
+        if low+1==high:
+            res=low+1
             break
-        i+=1
-    print()
+        if final[mid]<dist: low=mid
+        elif dist<final[mid]: high=mid
+    print(res)

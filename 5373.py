c9bd35e2b44cc614d4a61b174373e275b1268e83
hwany8677 <19703339+hwany8677@users.noreply.github.com>
2025-04-10 18:13:02 +0900

    4월 8일 집

---
+++ 5373.py
@@ -0,0 +1,7 @@
+#‘+’ for a clockwise rotation (when looking directly at the face that is rotated), 
+#‘-’ for a counterclockwise rotation.
+input=open(0).readline
+for _ in range(int(input())):
+    n=int(input())
+    buf=input().rstrip().split()
+    

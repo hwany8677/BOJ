dfcf927b8348fdea559a36f00b2e931d8f1b1059
hwany8677 <19703339+hwany8677@users.noreply.github.com>
2025-09-08 00:45:34 +0900

    9월 8일 집 (1874 청산, CLASS 2 完)

--- 1874.py
+++ 1874.py
@@ -1,30 +1,38 @@
 #pop연산이 중요해짐
 #연산이 남았는데 스택이 빌 시 -> NO
+#2025년 9월 8일 재작성
 from sys import stdin
 input=stdin.readline
 n=int(input())
 order=[]
 for _ in range(n): order.append(int(input()))
-length=0
-to_add=1
-stack=[]
-instructions=[]
+length=1
+to_add=2
+stack=[1]
+instructions=["+"]
 i=0 #i=order
 while(i<n):
-    stack.append(to_add)
-    instructions.append('+')
-    length+=1
-    to_add+=1
-    if stack[length-1]>=order[i]:
-        for j in range(length-1,-1,-1):
-            if stack[j]==order[i]:
-                stack.pop()
-                instructions.append('-')
-                length-=1
-                i+=1
-                break
-            else:
-                stack.pop()
-                instructions.append('-')
-                length-=1
+    #push()하기 전, 윗부분이 같을 때 pop(), 아니면 push()
+    #윗부분이 이미 원하는 수를 뛰어넘었으면 그 즉시 NO를 출력하고 종료.
+    # print(f"Order: {order[i]}")
+    # print(f"Stack: {stack}")
+    # print(f"Last inst.: {instructions[len(instructions)-1]}")
+    if length==0:
+        stack.append(to_add)
+        instructions.append("+")
+        to_add+=1
+        length+=1
+    elif stack[length-1]>order[i]:
+        print("NO")
+        exit(0)
+    elif stack[length-1]==order[i]:
+        instructions.append("-")
+        stack.pop()
+        length-=1
+        i+=1
+    else:
+        stack.append(to_add)
+        instructions.append("+")
+        to_add+=1
+        length+=1
 for inst in instructions: print(inst)
